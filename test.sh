53bfd0377b68927c00ee19b67886e148219be495
Mark Simulacrum <mark.simulacrum@gmail.com>
2017-05-28 16:00:21 -0600

    Move test.sh to test.example.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker run --rm -v `pwd`:/dir -v `pwd`/{{GIT_DIRECTORY}}:/source \
-	-e RUSTC=/dir/$RUSTC_RELATIVE -e CARGO_RELATIVE jimmycuadra/rust \
-	bash -c 'cd /source && rm -fr target ; /dir/$CARGO_RELATIVE test' \
-	2>&1 | rg -q '{{ERROR MESSAGE}}'
